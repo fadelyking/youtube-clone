cba489370a6da393febbd1eb0feb7a184f7c4b21
fadelyking <fadely195@gmail.com>
2024-05-07 11:22:26 +0300

    youtube sidebar planning

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+export default function Sidebar() {
+    return (
+        <>
+            {/* Home Shorts & Subscription */}
+            {/* Separator */}
+            {/* You - Header */}
+            {/* Your channel & History & Playlists & Your videos */}
+            {/* Separator */}
+            {/* Subscriptions - Header */}
+            {/* Separator */}
+            {/* Explore - Header */}
+            {/* Trending Shorts & Subscription */}
+            {/* More From Youtube - Header */}
+        </>
+    );
+}
